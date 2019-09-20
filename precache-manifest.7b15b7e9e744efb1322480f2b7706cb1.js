9b2c0d4bcd805ee12e1e880fad5f171e1da26aca
Horacio Garcia <garciahor@gmail.com>
2019-09-20 09:55:48 -0400

    Updates

---
+++ precache-manifest.7b15b7e9e744efb1322480f2b7706cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b89fb33be24f0e73cda5a57eb8a1e76",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "33f3c9565b34c2712a0e",
+    "url": "/clicky-game/static/css/main.54c7b65b.chunk.css"
+  },
+  {
+    "revision": "62d941b79739b579913e",
+    "url": "/clicky-game/static/js/2.2a4c734b.chunk.js"
+  },
+  {
+    "revision": "33f3c9565b34c2712a0e",
+    "url": "/clicky-game/static/js/main.acb82c15.chunk.js"
+  },
+  {
+    "revision": "b91c3118b753ce28e6fb",
+    "url": "/clicky-game/static/js/runtime~main.a7166744.js"
+  }
+]);
